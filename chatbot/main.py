6ce8b30025b6975254e9a30d1ddba8b5a303cebc
unmeshsupekar3 <unmeshsupekar3@gmail.com>
2024-09-11 21:10:36 +0530

    [FEATURE] added chatbot folder

---
+++ chatbot/main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+import streamlit as st
+
+
+file_formats = {
+    "csv": pd.read_csv,
+    "xls": pd.read_excel,
+    "xlsx": pd.read_excel,
+    "xlsm": pd.read_excel,
+    "xlsb": pd.read_excel,
+}
+@st.cache_data(ttl="2h")
+def load_data(uploaded_file):
+    try:
+        ext = os.path.splitext(uploaded_file.name)[1][1:].lower()
+    except:
+        ext = uploaded_file.split(".")[-1]
+    if ext in file_formats:
+        return file_formats[ext](uploaded_file)
+    else:
+        st.error(f"Unsupported file format: {ext}")
+        return None
+
+# Read the Pandas DataFrame
+df = load_data(uploaded_file)
